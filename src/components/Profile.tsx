e1958103e7236d535f8580dc862da934f66b0a6b
louremipsum <myself.vinayak2@gmail.com>
2024-06-07 10:25:02 +0530

    chore(minor): getting user data from Supabase auth server instead of session since this is more secure

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,28 @@
 "use client";
 
 import Image from "next/image";
-import { Session } from "@supabase/supabase-js";
+import { createSupabaseBrowserClient } from "@/utils/supabase/client";
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
 
-const Profile = ({ session }: { session: Session | null }) => {
-  const user = session?.user;
-  console.log("User-> ", user);
+const Profile = () => {
+  const [data, setData] = useState<User | null>();
+  const supabase = createSupabaseBrowserClient();
+  useEffect(() => {
+    const getData = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setData(user);
+    };
+    getData();
+  }, [supabase.auth]);
 
   return (
     <div className="flex  items-center justify-between space-x-6 p-8">
       <Image
         className="h-20 w-20 flex-shrink-0 rounded-full bg-gray-300"
-        src={user?.user_metadata.avatar_url}
+        src={data?.user_metadata.avatar_url}
         alt="Profile picture"
         height={100}
         width={100}
@@ -19,10 +30,10 @@ const Profile = ({ session }: { session: Session | null }) => {
       <div className="flex-1 truncate">
         <div className="flex items-center space-x-3">
           <h3 className="truncate text-xl font-medium text-gray-900">
-            {user?.user_metadata?.full_name}
+            {data?.user_metadata?.full_name}
           </h3>
         </div>
-        <p className="mt-1 truncate text-sm text-gray-500">{user?.email}</p>
+        <p className="mt-1 truncate text-sm text-gray-500">{data?.email}</p>
       </div>
     </div>
   );
