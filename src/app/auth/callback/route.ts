ece0058aaeb8644ba6e3a6c5d58651e3e5b64e5d
louremipsum <myself.vinayak2@gmail.com>
2024-06-07 10:26:13 +0530

    chore

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import { createSupabaseServerClient } from "@/utils/supabase/server";
+import { createSupabaseServerClient } from "@/utils/supabase/server-client";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
