93f7920a7ad2051bb79f96768d68e042b2b6f413
louremipsum <myself.vinayak2@gmail.com>
2024-06-08 22:59:42 +0530

    style: Update button border color in Login component
    The button border color in the Login component was updated from "border-transparent" to "border-gray-200" to improve visual consistency.

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -22,7 +22,7 @@ const Login = (props: { nextUrl?: string }) => {
   return (
     <div className="flex justify-start p-4 ">
       <button
-        className="flex items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black bg-white hover:bg-gray-200 transition-all duration-300"
+        className="flex items-center px-8 py-3 border border-gray-200 text-base font-medium rounded-md text-black bg-white hover:bg-gray-200 transition-all duration-300"
         onClick={handleLogin}
       >
         <div className="pr-5">
