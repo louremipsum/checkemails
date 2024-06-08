6f06a3e97118c908afe1d340b7b6cdec4713dbad
louremipsum <myself.vinayak2@gmail.com>
2024-06-08 23:05:39 +0530

    refactor: Update EmailList component to accept emails, error, and isLoading as props
    The EmailList component has been refactored fetch emails from parent component and to accept the emails, error, and isLoading as props instead of using useState hooks.
    
    it was done as after fetching emails initially and then classify, the classified email had to be changed again but it couldnt be done as both happened in parent and child component

--- src/components/EmailList.tsx
+++ src/components/EmailList.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { emailDetailsType } from "@/app/types";
 import Loader from "@/components/Loader";
-import { useEffect, useState } from "react";
-import EmailCard from "./EmailCard";
+import { useState } from "react";
 import Drawer from "./Drawer";
+import EmailCard from "./EmailCard";
 
-const EmailList = ({ maxResults }: { maxResults: number }) => {
-  const [emails, setEmails] = useState<emailDetailsType[]>([]);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+type emailListType = {
+  emails: emailDetailsType[];
+  error: string | null;
+  isLoading: boolean;
+};
+
+const EmailList = ({ emails, error, isLoading }: emailListType) => {
   const [isSelected, setIsSelected] = useState<emailDetailsType | null>(null);
   const [isDrawerVisible, setDrawerVisibility] = useState<boolean>(false);
 
@@ -29,60 +32,39 @@ const EmailList = ({ maxResults }: { maxResults: number }) => {
     setDrawerVisibility(state);
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch(`/api/getEmails?maxResults=${2}`, {
-          method: "GET",
-        });
-        if (!response.ok) {
-          throw new Error("Failed to fetch emails");
-        }
-        const data = await response.json();
-        setEmails(data.emails);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError("An unexpected error occurred");
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [maxResults]);
-
   if (error) {
     return <div>Error: {error}</div>;
   }
   return (
     <div>
-      <h1 className="text-gray-500 text-3xl my-5">Recent Emails</h1>
-      {isLoading ? (
-        <Loader />
-      ) : emails.length === 0 ? (
-        <div className="text-lg my-8 h-48 flex items-center justify-center text-gray-600">
-          No emails are in your inbox
-        </div>
-      ) : (
-        <div onClick={(e) => handleEmailClick(e)}>
-          {emails.map((email) => (
-            <div key={email.index} data-id={email.index} className="email-card">
-              <EmailCard
-                from={email.from}
-                snippet={email.snippet}
-                plainText={email.plainText}
-                htmlText={email.htmlText}
-                index={email.index}
-                category={email.category}
-              />
-            </div>
-          ))}
-        </div>
-      )}
+      <div>
+        {isLoading ? (
+          <Loader />
+        ) : emails.length === 0 ? (
+          <div className="text-lg my-8 h-48 flex items-center justify-center text-gray-600">
+            No emails are in your inbox
+          </div>
+        ) : (
+          <div onClick={(e) => handleEmailClick(e)}>
+            {emails.map((email) => (
+              <div
+                key={email.index}
+                data-id={email.index}
+                className="email-card"
+              >
+                <EmailCard
+                  from={email.from}
+                  snippet={email.snippet}
+                  plainText={email.plainText}
+                  htmlText={email.htmlText}
+                  index={email.index}
+                  category={email.category}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
 
       <div>
         {isSelected && (
