bfbf61a0e82641480ec7bea325a11333ed50ad30
louremipsum <myself.vinayak2@gmail.com>
2024-06-08 11:49:13 +0530

    style: beautifu

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,14 +2,27 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center">
-      <h2 className="text-2xl font-bold text-red-600">Not Found</h2>
-      <p className="mt-3 text-lg text-gray-500">
-        Could not find requested resource
-      </p>
-      <Link href="/" className="mt-6 text-blue-500 hover:underline">
-        Return Home
-      </Link>
-    </div>
+    <section className="bg-slate-100 flex justify-center items-center h-screen">
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-yellow-500 ">
+            404
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-700 md:text-4xl ">
+            Something&apos;s missing.
+          </p>
+          <p className="mb-4 text-lg font-light  text-gray-500">
+            Sorry, we can&apos;t find that page. You&apos;ll find lots to
+            explore on the home page.{" "}
+          </p>
+          <Link
+            href="/"
+            className="inline-flex text-white bg-yellow-500 hover:bg-yellow-600 transition-all duration-300 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4"
+          >
+            Back to Homepage
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
