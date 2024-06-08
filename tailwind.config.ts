8ebfdf9b9589f2cd5965915f6dc2ca96fdb835bb
louremipsum <myself.vinayak2@gmail.com>
2024-06-08 10:37:55 +0530

    config: scale for 1.01 for EmailCard component on hover

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         bgImg: "url('/bg.svg')",
       },
+      scale: {
+        "101": "1.01",
+      },
     },
   },
   plugins: [],
