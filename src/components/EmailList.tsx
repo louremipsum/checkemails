2ba1d75101c4b296b933f0aacf8337cdbb7ba430
louremipsum <myself.vinayak2@gmail.com>
2024-06-09 14:02:59 +0530

    feat: URLSearchParams based state management on what email is opened in Drawer

--- src/components/EmailList.tsx
+++ src/components/EmailList.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { emailDetailsType } from "@/app/types";
 import Loader from "@/components/Loader";
-import { useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import Drawer from "./Drawer";
 import EmailCard from "./EmailCard";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import Link from "next/link";
 
 type emailListType = {
   emails: emailDetailsType[];
@@ -14,22 +16,45 @@ type emailListType = {
 const EmailList = ({ emails, error, isLoading }: emailListType) => {
   const [isSelected, setIsSelected] = useState<emailDetailsType | null>(null);
   const [isDrawerVisible, setDrawerVisibility] = useState<boolean>(false);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  const handleEmailClick = (e: any) => {
-    const target = e.target.closest(".email-card");
-    if (!target) return;
-    const emailId = target.getAttribute("data-id");
-    const selectedEmail = emails.find(
-      (email) => email.index.toString() === emailId
-    );
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set(name, value);
+
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  const selectedEmail = useMemo(
+    () =>
+      emails.find(
+        (email) => email.index === parseInt(searchParams.get("id") || "") - 1
+      ),
+    [emails, searchParams]
+  );
+
+  const handleEmailClick = useCallback(() => {
     if (selectedEmail) {
       setIsSelected(selectedEmail);
       setDrawerVisibility(true);
     }
-  };
+  }, [selectedEmail]);
 
-  const handleDrawerVisibility = (state: boolean) => {
-    setDrawerVisibility(state);
+  useEffect(() => {
+    handleEmailClick();
+  }, [handleEmailClick]);
+
+  const ResetDrawerVisibility = () => {
+    setIsSelected(null);
+    router.push(pathname);
+    setDrawerVisibility(false);
   };
 
   if (error) {
@@ -45,23 +70,25 @@ const EmailList = ({ emails, error, isLoading }: emailListType) => {
             No emails are in your inbox
           </div>
         ) : (
-          <div onClick={(e) => handleEmailClick(e)}>
-            {emails.map((email) => (
-              <div
-                key={email.index}
-                data-id={email.index}
-                className="email-card"
-              >
-                <EmailCard
-                  from={email.from}
-                  snippet={email.snippet}
-                  plainText={email.plainText}
-                  htmlText={email.htmlText}
-                  index={email.index}
-                  category={email.category}
-                />
-              </div>
-            ))}
+          <div>
+            {emails.map((email) => {
+              const queryString = createQueryString(
+                "id",
+                (email.index + 1).toString()
+              );
+              return (
+                <Link href={`${pathname}?${queryString}`} key={email.index}>
+                  <EmailCard
+                    from={email.from}
+                    snippet={email.snippet}
+                    plainText={email.plainText}
+                    htmlText={email.htmlText}
+                    index={email.index}
+                    category={email.category}
+                  />
+                </Link>
+              );
+            })}
           </div>
         )}
       </div>
@@ -70,7 +97,7 @@ const EmailList = ({ emails, error, isLoading }: emailListType) => {
         {isSelected && (
           <Drawer
             isDrawerVisible={isDrawerVisible}
-            handleDrawerVisibility={handleDrawerVisibility}
+            handleDrawerVisibility={ResetDrawerVisibility}
             {...isSelected}
           />
         )}
