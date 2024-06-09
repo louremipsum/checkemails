d4b273c872263447bb54fbaf6a70df957dd13f19
louremipsum <myself.vinayak2@gmail.com>
2024-06-09 14:06:28 +0530

    style(minor): Update Home component to set a fixed height for better page layout
    The Home component has been refactored to set a fixed height for the container div, improving the overall page layout. This change ensures that the content is properly displayed within the div and prevents overflow issues.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ const Home = () => {
   };
 
   return (
-    <div className="bg-white overflow-hidden">
+    <div className="bg-white overflow-hidden h-screen">
       <header className="absolute inset-x-0 top-0 z-50">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <p className="text-xl dark:text-gray-700 fixed m-16 left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:bg-amber-700/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 ">
